fd244beeb5deab8eca40fe0580378e2edf80a080
thinhbuihong <thinhbuihong.ma@gmail.com>
2021-03-22 19:57:07 +0700

    custom authentication middleware server+ user register route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleWare.js';
 const userRoutes = express.Router();
 
+userRoutes.route('/').post(registerUser);
 userRoutes.route('/login').post(authUser);
+userRoutes.route('/profile').get(protect, getUserProfile);
 
 export default userRoutes;
